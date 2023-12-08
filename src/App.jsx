87de953a058d48bca3d07278ceb032db38014689
Priyanka Mukherjee <rhea.uco@gmail.com>
2023-12-09 02:50:31 +0530

    menu built and hover effects are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="menu">
+        <div className="menu-item">
+          <div className="item-name">HydroOasis</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">AlgaZen</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">NeuroBoost</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">EcoElevate</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">HerbaVigor</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">OhmAlchemy</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="item-name">FruitFuse</div>
+          <div className="item-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vero veritatis ducimus eius voluptatem totam qui aspernatur illum
+            magni ipsam?
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
